6fe8cec7d07f35d93fdf517ce60e68a633dcad13
biniyam-rgb <binigebrie853@gmail.com>
2026-03-25 00:14:36 +0300

    making the product detail style

--- amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
+++ amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
@@ -3,8 +3,9 @@ import LayOut from '../../Component/LayOut/LayOut'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { productUrl } from '../../Api/EndPoint'
-import ProductCard from '../../Component/Product/ProductCard'
 import Loader from '../../Component/Loader/Loader'
+import { Rating } from '@mui/material'
+import Classes from './ProductDetail.module.css'
 
 export default function ProductDetail() {
   const { productId } = useParams()
@@ -30,12 +31,25 @@ export default function ProductDetail() {
       {isLoading ? (
         <Loader />
       ) : product && (
-        <ProductCard
-          product={product}
-          flex={true}
-          renderDesc={true}
-          renderAdd={true}
-        />
+        <div className={Classes.container}>
+          {/* Left - Image */}
+          <div className={Classes.image__section}>
+            <img src={product.image} alt={product.title} className={Classes.image} />
+          </div>
+
+          {/* Right - All info */}
+          <div className={Classes.info__section}>
+            <h1 className={Classes.title}>{product.title}</h1>
+            <div className={Classes.rating}>
+              <Rating value={product.rating?.rate} precision={0.1} readOnly />
+              <span>{product.rating?.count} ratings</span>
+            </div>
+            <hr />
+            <p className={Classes.price}>${product.price?.toFixed(2)}</p>
+            <p className={Classes.description}>{product.description}</p>
+            <button className={Classes.cart__btn}>Add to Cart</button>
+          </div>
+        </div>
       )}
     </LayOut>
   )
